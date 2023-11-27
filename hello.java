d1f0332391406b0c7961bc15b5959c9b7428c20a
bijjampoorna@gmail.com <bijjampoorna@gmail.com>
2023-11-27 20:05:12 +0530

    hello.java file modified by dev2

--- hello.java
+++ hello.java
@@ -1,11 +1,11 @@
-class java
+class java #change made by dev2
 {
-   public static void main
+   public static void main #change made by dev2
    {
 	
-	system.out.println("hello from dev1")
+	system.out.println("hello from dev2")
 
    }
 
 
-}
+}#change made by dev2
